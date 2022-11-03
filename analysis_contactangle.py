Major bug: conversionZ was off by factor of 2

analysis_contactangle.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from matplotlib import pyplot as plt
 import json
@@ -38,22 +40,24 @@ procStatsJsonPath = r'C:\Users\HOEKHJ\Dev\InterferometryPython\export\PROC_20221
 csvPathAppend = r'csv'
 flipData = True
 
-analyzeImages = [500]
+analyzeImages = np.arange(90, 900, 50)
 
 with open(procStatsJsonPath, 'r') as f:
     procStats = json.load(f)
 
-for idx in analyzeImages:
-    originalPath = procStats["analysis"][str(idx)]["wrappedPath"]
+exportData = np.zeros_like(analyzeImages, dtype='float')
+
+for idx, imageNumber in enumerate(analyzeImages):
+    originalPath = procStats["analysis"][str(imageNumber)]["wrappedPath"]
     dataPath = os.path.join(os.path.dirname(originalPath), csvPathAppend, os.path.basename(originalPath))
     print(dataPath)
 
-    y = np.loadtxt(dataPath, delimiter=",")
+    y = np.loadtxt(dataPath, delimiter=",") / 10 # y is in um. 10 is error in DATA SAVED?
     if flipData:
         y = -y + max(y)
-    x = np.arange(0, len(y))
-    print(len(x), len(y))
-    print(type(x), type(y))
+
+    conversionXY = procStats["conversionFactorXY"]
+    x = np.arange(0, len(y)) * conversionXY * 1000  # x is now in um
 
     fig, ax = plt.subplots()
     ax.scatter(x, y)
@@ -69,8 +73,8 @@ for idx in analyzeImages:
     selected_regions = highlighter.mask
     xrange2, yrange2 = x[selected_regions], y[selected_regions]
 
-    print(xrange1, yrange1)
-    print(xrange2, yrange2)
+    # print(xrange1, yrange1)
+    # print(xrange2, yrange2)
 
     coef1 = np.polyfit(xrange1, yrange1, 1)
     poly1d_fn1 = np.poly1d(coef1)
@@ -78,12 +82,35 @@ for idx in analyzeImages:
     coef2 = np.polyfit(xrange2, yrange2, 1)
     poly1d_fn2 = np.poly1d(coef2)
 
+    # print(coef1, coef2)
+    angleRad = math.atan((coef1[0]-coef2[0])/(1+coef1[0]*coef2[0]))
+    angleDeg = math.degrees(angleRad)
+
+    # print(f"{angleRad=}")
+    # print(f"{angleDeg=}")
+
     fig, ax = plt.subplots()
-    ax.scatter(x, y)
-    ax.plot(x, poly1d_fn1(x))
-    ax.plot(x, poly1d_fn2(x))
+    ax.scatter(x, y, label=f'Raw data {os.path.basename(originalPath)}')
+    ax.scatter(xrange1, yrange1, color='green', label='Selected data line 1')
+    ax.scatter(xrange2, yrange2, color='green', label='Selected data line 2')
+    ax.plot(x, poly1d_fn1(x), color='red', linewidth=3, label='Linear fit 1')
+    ax.plot(x, poly1d_fn2(x), color='red', linewidth=3, label='Linear fit 2')
+    ax.set_title(f"{angleDeg=}")
+    ax.set_xlabel("[um]")
+    ax.set_ylabel("[um]")
+    ax.set_xlim([x[0], x[-1]])
+    ax.set_ylim([y[0], y[-1]])
+
+
+
+    fig.savefig(os.path.join(os.path.dirname(originalPath), f"angleFitting_{os.path.splitext(os.path.basename(originalPath))[0]}.png"), dpi=300)
+    exportData[idx] = angleDeg
 
     plt.show()
+
+
+exportData.tofile(os.path.join(os.path.dirname(procStatsJsonPath), f"angleFittingData.csv"), sep=',')
+print(exportData)
 
 
 