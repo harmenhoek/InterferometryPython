Major bug fixes: conversionZ, wrapped space normalization
Do not use older versions!

analysis_contactangle.py
@@ -37,12 +37,15 @@ class Highlighter(object):
         return mask
 
 
-procStatsJsonPath = r'C:\Users\HOEKHJ\Dev\InterferometryPython\export\PROC_20221103145006\PROC_20221103145006_statistics.json'
-csvPathAppend = r'csv'
+procStatsJsonPath = '/Users/harmenhoek/Dev/InterferometryPython/export/PROC_20221104130005/PROC_20221104130005_statistics.json'
+csvPathAppend = r''
 flipData = True
 
-analyzeImages = np.arange(41*4, 900, 4)
-analyzeImages = analyzeImages[3:4]
+# 1 slice: Contact angle = -1.6494950309356011 degrees.
+# 11 slices: -1.650786783947852 degrees.
+
+analyzeImages = np.array([0])
+
 
 with open(procStatsJsonPath, 'r') as f:
     procStats = json.load(f)
@@ -91,9 +94,10 @@ try:
         fig, ax = plt.subplots()
         ax.scatter(x, y)
         highlighter = Highlighter(ax, x, y)
-        plt.draw()
-        plt.waitforbuttonpress(0)  # this will wait for indefinite time
-        plt.close(fig)
+        plt.show()
+        # plt.draw()
+        # plt.waitforbuttonpress(0)  # this will wait for indefinite time
+        # plt.close(fig)
         selected_regions = highlighter.mask
         xrange1, yrange1 = x[selected_regions], y[selected_regions]
 

general_functions.py
@@ -186,6 +186,7 @@ def conversion_factors(config):
         logging.info(f"Lens preset '{config.getfloat('LENS_PRESETS', config.get('GENERAL', 'LENS_PRESET'))}' is used.")
     except ValueError:
         logging.error(f"The set lens preset '{config.get('GENERAL', 'LENS_PRESET')}' is not in LENS_PRESETS.")
+        exit()
 
     unitXY = config.get('GENERAL', 'UNIT_XY')
     if unitXY not in units:
@@ -197,7 +198,7 @@ def conversion_factors(config):
     if unitZ == 'pi':
         conversionFactorZ = 1
     else:
-        conversionFactorZ = (config.getfloat('GENERAL', 'WAVELENGTH')) / (8 * config.getfloat('GENERAL', 'REFRACTIVE_INDEX'))  # 1 pi = lambda / (4n), 1/2 since we have units of 2pi. this is conversion factor in pi --> m
+        conversionFactorZ = (config.getfloat('GENERAL', 'WAVELENGTH')) / (2 * config.getfloat('GENERAL', 'REFRACTIVE_INDEX')) / (2 * np.pi)  # 1 period of 2pi = lambda / (4n). /2pi since our wrapped space is in absolute units, not pi
         if unitZ not in units:
             raise ValueError(f"Desired unit {unitZ} is not valid. Choose, nm, um, mm or m.")
         conversionFactorZ = conversionFactorZ * conversionsZ[units.index(unitZ)]  # apply unit conversion

line_method.py
@@ -5,6 +5,24 @@ import logging
 import os
 from general_functions import image_resize
 
+
+def normalize_wrappedspace(signal, threshold):
+    '''
+    Local normalization of the wrapped space. Since fringe pattern is never ideal (i.e. never runs between 0-1) due
+    to noise and averaging errors, the wrapped space doesn't run from -pi to pi, but somewhere inbetween. By setting
+    this value to True, the wrapped space is normalized from -pi to pi, if the stepsize is above a certain threshold.
+    :param signal: wrapped signal, i.e. stepped-wise signal between -pi and pi
+    :param threshold: if step is larger than this value, the neighboring peaks are pulled to -pi and pi. should be
+        0<Threshold<=2pi
+    :return: normalized signal
+    '''
+    # calculate differential. +0 since len(diff) = len(signal)-1
+    diff = np.append(np.diff(signal), 0)
+    # adjust peaks if outside of threshold
+    signal[diff > threshold] = np.pi
+    signal[np.where(diff > threshold)[0] - 1] = -np.pi
+    return signal
+
 def intersection_imageedge(a, b, limits):
     '''
     For a given image dimension (limits) and linear line (y=ax+b) it determines which borders of the image the line
@@ -262,7 +280,6 @@ def method_line(config, **kwargs):
     AlignmentPoint = (im_gray.shape[0] // 2, im_gray.shape[1] // 2)  # take center of image as alignment
     profiles_aligned = align_arrays(all_coordinates, profiles, AlignmentPoint)
 
-
     # TODO filtering here to disregard datapoints (make nan) that have little statistical significance
     def filter_profiles(profiles_aligned, profile):
         nanValues = np.sum(np.isnan(profiles_aligned), axis=0)
@@ -273,9 +290,6 @@ def method_line(config, **kwargs):
 
     if config.getboolean("LINE_METHOD_ADVANCED", "FILTER_STARTEND"):
         profile = filter_profiles(profiles_aligned, profile)
-
-
-
 
     logging.info("Profiles are aligned and average profile is determined.")
 
@@ -294,17 +308,18 @@ def method_line(config, **kwargs):
         # mask = smooth_step(mask, highPassBlur)
     profile_fft = profile_fft * mask
 
+
     profile_filtered = np.fft.ifft(profile_fft)
     logging.info("Average profile is filtered in the Fourier space.")
 
-
-    # clahe = cv2.createCLAHE()
-    # profile_filtered = cv2.cvtColor(profile_filtered, cv2.COLOR_BGR2GRAY)
-    # profile_filtered = clahe.apply(profile_filtered)
-
+    # calculate the wrapped space
     wrapped = np.arctan2(profile_filtered.imag, profile_filtered.real)
 
-
+    # local normalization of the wrapped space. Since fringe pattern is never ideal (i.e. never runs between 0-1) due
+    # to noise and averaging errors, the wrapped space doesn't run from -pi to pi, but somewhere inbetween. By setting
+    # this value to True, the wrapped space is normalized from -pi to pi, if the stepsize is above a certain threshold.
+    if config.getboolean("LINE_METHOD_ADVANCED", "NORMALIZE_WRAPPEDSPACE"):
+        wrapped = normalize_wrappedspace(wrapped, config.getfloat("LINE_METHOD_ADVANCED", "NORMALIZE_WRAPPEDSPACE_THRESHOLD"))
 
     unwrapped = np.unwrap(wrapped)
     logging.info("Average slice is wrapped and unwrapped")
@@ -316,20 +331,11 @@ def method_line(config, **kwargs):
     unwrapped_converted = unwrapped * conversionFactorZ
     logging.debug('Conversion factor for Z applied.')
 
-    # # TODO TEMP
-    # from matplotlib import pyplot as plt
-    # fig, ax = plt.subplots()
-    # ax.plot(wrapped, '.-')
-    # fig2, ax2 = plt.subplots()
-    # ax2.plot(profile, '.-')
-    # plt.show()
-
-
     from plotting import plot_lineprocess, plot_profiles, plot_sliceoverlay, plot_unwrappedslice
     fig1 = plot_profiles(config, profiles_aligned)
     fig2 = plot_lineprocess(config, profile, profile_filtered, wrapped, unwrapped)
     fig3 = plot_sliceoverlay(config, all_coordinates, im_raw)
-    fig4 = plot_unwrappedslice(config, unwrapped_converted, profiles_aligned, conversionFactorXY, unitXY, unitZ)
+    fig4 = plot_unwrappedslice(config, unwrapped_converted, profiles_aligned, conversionFactorXY, unitXY, unitZ, wrapped, unwrapped)
     logging.info(f"Plotting done.")
 
     # Saving

plotting.py
@@ -142,12 +142,21 @@ def plot_sliceoverlay(config, coordinates, image):
     fig.tight_layout()
     return fig
 
-def plot_unwrappedslice(config, unwrapped_object, profiles, conversionFactorXY, unitXY, unitZ):
+def plot_unwrappedslice(config, unwrapped_object, profiles, conversionFactorXY, unitXY, unitZ, wrapped, unwrapped):
     fig = plt.figure(figsize=(8, 5))
     ax = fig.add_subplot(111)
-    ax.imshow(profiles, cmap='gray', extent=np.array([0, conversionFactorXY*len(unwrapped_object), 0, np.max(unwrapped_object)]), aspect='auto')
-    ax.plot(np.linspace(0, conversionFactorXY*len(unwrapped_object), len(unwrapped_object)), unwrapped_object, label='unwrapped', color='red')
+    # ax.imshow(profiles, cmap='gray', extent=np.array([0, conversionFactorXY*len(unwrapped_object), 0, np.max(unwrapped_object)]), aspect='auto')
+    ax.imshow(profiles, cmap='gray', extent=np.array([0, conversionFactorXY*len(unwrapped), 0, np.max(unwrapped)]), aspect='auto')
+    # ax.plot(np.linspace(0, conversionFactorXY*len(unwrapped_object), len(unwrapped_object)), unwrapped_object, label='unwrapped', color='red')
+    ax.plot(np.linspace(0, conversionFactorXY*len(unwrapped), len(unwrapped)), unwrapped , color='red')
     ax.set_xlabel(f'[{unitXY}]')
     ax.set_ylabel(f'[{unitZ}]')
+
+    # ax2 = ax.twinx()
+    # # ax2.plot(np.linspace(0, conversionFactorXY*len(profile), len(profile)), profile)
+    # ax2.plot(np.linspace(0, conversionFactorXY*len(wrapped), len(wrapped)), wrapped)
+
+    plt.xlim([1140, 1205])  # TODO TEMP
+
     fig.tight_layout()
     return fig